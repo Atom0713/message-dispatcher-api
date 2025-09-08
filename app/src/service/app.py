2312285254fb897c84a6310ac4f21592ce8350fe
Artem Sliusarenko <sliusarenko.artem@gmail.com>
2025-09-08 08:33:11 +0200

    create resources on service startup & handle arbitrary exceptions on message submit action

--- app/src/service/app.py
+++ app/src/service/app.py
@@ -1,10 +1,23 @@
-from fastapi import APIRouter, Depends, FastAPI
+import structlog
+from fastapi import APIRouter, Depends, FastAPI, Response
+from fastapi.responses import JSONResponse
 
 from service.schemas import MessageContent, Messages, MessagesQuery, query_params
+from service.services import RecipientMessagesService
+from service.db import create_table, dynamodb
+from contextlib import asynccontextmanager
 
 app = FastAPI()
 
 api_v1_router = APIRouter(prefix="/api/v1")
+
+logger = structlog.get_logger()
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    create_table(dynamodb)
+    yield
 
 
 @api_v1_router.get("/")
@@ -14,7 +27,12 @@ def read_root():
 
 @api_v1_router.post("/recipients/{recipient_id}/messages")
 def submit_message(recipient_id: str, message_content: MessageContent) -> dict:
-    return {"delivery": "pending"}
+    try:
+        RecipientMessagesService().create(recipient_id, message_content.content)
+    except Exception as e:
+        logger.exception(e)
+        return JSONResponse(content={"message": "INTERNAL_SERVER_ERROR"}, status_code=500)
+    return Response(status_code=200)
 
 
 @api_v1_router.delete("/recipients/{recipient_id}/messages/{message_id}")
